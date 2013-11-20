9ba7b866e14883083deb573913260a281798bc6d
MakerSquare Student <students@themakersquare.com>
2013-11-20 11:41:57 -0600

    Add failing test for the non-existant topping method in the Pie class

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -5,6 +5,18 @@ require_relative '../pizza'
 
 describe Pizza::Pie do
 
+	describe '.initialize' do
+		it 'records all of the toppings' do
+			toppings = [
+				Pizza::Topping.new('mushrooms', vegetarian: true),
+				Pizza::Topping.new('pepperoni')
+			]
+			pizza = Pizza::Pie.new(toppings)
+
+			expect(pizza.toppings).to eq(toppings)
+		end
+	end
+
 end 
 
 describe Pizza::Topping do
